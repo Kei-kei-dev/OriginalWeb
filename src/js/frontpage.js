28e4f11cf35ba123e925406e9f8e8df29bb8e257
Kei-kei-dev <124547360+Kei-kei-dev@users.noreply.github.com>
2024-03-08 12:45:45 +0800

    Initial commit

---
+++ src/js/frontpage.js
@@ -0,0 +1,20 @@
+// スクロール検知のフェードイン
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+            // それぞれのblockクラスに対して…
+            $(".storyItems-left, .storyItems-right, .img-content, .solveItems-left, .solveItems-right").each(function () {
+                // それぞれのblockクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+                // スクロールした量が要素の高さを上回ったら
+                // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 200) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // 第一候補
+        // 'main': '#79A1D4',
+        'main': '#78BBE6',
+        'sub': '#E3F2FD',
+        'light-beige': '#f8f6f3',
+        'point': '#ff5c4d',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
